23e8f505b8fed4f92b9bd69c950b85d170d01185
ferlongin <ferlongin@hotmail.com>
2023-04-23 11:46:11 -0300

    sobre mi listo :)

--- src/Pages/SobreMi/Components/PersonalInfo.jsx
+++ src/Pages/SobreMi/Components/PersonalInfo.jsx
@@ -1,11 +1,34 @@
-function PersonalInfo(props) {
-  return (
-    <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-      <h2 className="text-xl font-bold mb-4">{props.name}</h2>
-      <p className="text-gray-700 text-base mb-2">Edad: {props.age}</p>
-      <p className="text-gray-700 text-base mb-2">Locación: {props.location}</p>
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PersonalInfo = ({ name, surname, email, linkedin, phone, location, imageSrc }) => (
+  <div className="bg-white rounded-lg shadow-md p-6 m-5 flex items-center">
+    <div className="mr-6">
+      <img src={imageSrc} className="w-32 h-32 rounded-full object-cover" />
     </div>
-  );
-}
+    <div>
+      <h2 className="text-lg font-medium text-gray-900">{name} {surname}</h2>
+      <p className="text-sm font-medium text-gray-600">{location}</p>
+      <div className="mt-4">
+        <h3 className="text-sm font-medium text-gray-600">Contact Information</h3>
+        <div className="mt-2">
+          <p className="text-sm text-gray-500"><span className="font-medium">Email:</span> {email}</p>
+          <p className="text-sm text-gray-500"><span className="font-medium">Phone:</span> {phone}</p>
+          <p className="text-sm text-gray-500"><span className="font-medium">LinkedIn:</span> <a href={linkedin} className="text-blue-600">{linkedin}</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+/*PersonalInfo.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  linkedin: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string.isRequired,
+};*/
 
 export default PersonalInfo;
--- src/Pages/SobreMi/SobreMi.jsx
+++ src/Pages/SobreMi/SobreMi.jsx
@@ -1,11 +1,12 @@
 import PersonalInfo from "./Components/PersonalInfo";
-
+import Foto_CV from "../../assets/Foto_CV.jpg"
 
 export const SobreMi = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <PersonalInfo name="John Doe" age="27" location="New York" />
-      <PersonalInfo name="Jane Smith" age="35" location="Los Angeles" />
+    <div className="container mx-auto px-4 py-8 max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl text-white font-bold border-b-4 border-[#564dfb] p-2 inline">Sobre mi</h1>
+      <PersonalInfo name="Cecilia Ayelen" surname="Martinez" email="" linkedin="" phone="" location="Buenos Aires" imageSrc={Foto_CV}/>
+      <PersonalInfo name="Fernando José" surname="Longin" email="ferlongin@hotmail.com" linkedin="www.linkedin.com/in/fernando-josé-longin" phone="+54 9 11 5586-5874" location="Buenos Aires" imageSrc={Foto_CV}/>
     </div>
   );
 };
