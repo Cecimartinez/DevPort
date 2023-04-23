4159b505eab487921e9c4cf1b929c7a0d4e3e654
ferlongin <ferlongin@hotmail.com>
2023-04-23 16:59:57 -0300

    add images to formacion

--- src/Pages/Formacion/Components/FormacionItem.jsx
+++ src/Pages/Formacion/Components/FormacionItem.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
-const FormacionItem = ({ año, institucion, titulo, descripcion }) => {
+const FormacionItem = ({ año, institucion, titulo, imagen}) => {
     return (
       <ol className="bg-white rounded-lg shadow-md p-6 m-5 flex items-center text-black">
-        <li className="flex justify-between w-full">
+        <li className="flex w-full">
+          <div className="flex-shrink-0 mr-4">
+            <img src={imagen} className="w-30 h-16" />
+          </div>
           <div>
             <p className="text-lg font-semibold">{institucion}</p>
             <p className="text-m font-semibold">{titulo}</p>
           </div>
-          <p className="text-sm mt-2 p-2 font-semibold bg-blue-900 rounded-md text-white">{año}</p>
+          <p className="text-sm p-2 font-semibold bg-blue-900 rounded-md text-white ml-auto">{año}</p>
         </li>
-        <p className="mb-2">{descripcion}</p>
       </ol>
     );
-  };
-  
-  
+};
 
 FormacionItem.propTypes = {
   año: PropTypes.string.isRequired,
   institucion: PropTypes.string.isRequired,
   titulo: PropTypes.string.isRequired,
+  imagen: PropTypes.string,
 };
 
 export default FormacionItem;
--- src/Pages/Formacion/Formacion.jsx
+++ src/Pages/Formacion/Formacion.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import FormacionItem from "./Components/FormacionItem"
+import Foto_CV from "../../assets/Foto_CV.jpg"
 
 const Formacion = () => {
+  const experiencia = [
+    {
+      año: "2023 - Present",
+      institucion: "ExxonMobil",
+      titulo: "Software Analyst Trainee",
+    }
+  ];
+
   const data = [
     {
       año: "2021 - 2024",
@@ -26,8 +35,20 @@ const Formacion = () => {
   return (
     <div>
       <div id="formacion" className="container mx-auto px-4 py-8 max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl text-white font-bold border-b-4 border-[#564dfb] p-2 inline">
+          Experiencia
+        </h1>
+        {experiencia.map((item, idx) => (
+          <FormacionItem
+            key={idx}
+            año={item.año}
+            institucion={item.institucion}
+            titulo={item.titulo}
+            imagen={Foto_CV}
+          />
+        ))}
         <h1 className="text-4xl text-white font-bold border-b-4 border-[#564dfb] p-2 inline">
-          Formación
+          Educación
         </h1>
         {data.map((item, idx) => (
           <FormacionItem
@@ -35,10 +56,11 @@ const Formacion = () => {
             año={item.año}
             institucion={item.institucion}
             titulo={item.titulo}
+            imagen={item.imagen}
           />
         ))}
         <h1 className="text-4xl text-white font-bold border-b-4 border-[#564dfb] p-2 inline mt-8">
-        Certificaciones
+        Licencias y Certificaciones
         </h1>
         {certificaciones.map((item, idx) => (
           <FormacionItem
@@ -46,6 +68,7 @@ const Formacion = () => {
             año={item.año}
             institucion={item.institucion}
             titulo={item.titulo}
+            imagen={item.imagen}
           />
       ))}
       </div>
