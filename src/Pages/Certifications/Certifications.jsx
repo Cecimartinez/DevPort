629f13b07e7b1d4b1d0441665633c9610ffdf9a3
ferlongin <ferlongin@hotmail.com>
2023-05-05 21:59:45 -0300

    certifications carousel

--- src/Pages/Certifications/Certifications.jsx
+++ src/Pages/Certifications/Certifications.jsx
@@ -1,10 +1,30 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { CardCertifications } from "./Components/CardCertifications";
 
 export const Certifications = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+  
   return (
-    <div className=" flex  flex-col items-center justify-center mx-4 ">
-      <CardCertifications />
-      <CardCertifications />
+    <div style={{ width: "80%", margin: "0 auto", marginTop: "100px" }}>
+      <Slider {...settings}>
+        <div><CardCertifications /></div>
+        <div><CardCertifications /></div>
+        <div><CardCertifications /></div>
+        <div><CardCertifications /></div>
+        <div><CardCertifications /></div>
+        <div><CardCertifications /></div>
+      </Slider>
     </div>
+
   );
 };
--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -2,10 +2,7 @@ import Experience from "./Components/Experience";
 
 export const Skills = () => {
   return (
-    <div
-      id="skills"
-      className="container mx-auto px-4 py-8 max-w-[1040px] m-auto md:pl-20 p-4 "
-    >
+    <div id="skills" className="container mx-auto px-4 py-8 max-w-[1040px] m-auto md:pl-20 p-4 ">
       <Experience />
     </div>
   );
