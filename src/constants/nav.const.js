942f9ea8ffef362b2b875d16350c845857c50428
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-05-12 21:24:15 -0300

    commit

--- src/constants/nav.const.js
+++ src/constants/nav.const.js
@@ -1,6 +1,6 @@
 import { ROUTE_PATHS } from "../router/utils/routePaths"
 export const NAVITEMS = [
   { label: "Home", id: "item-nav", propName: ROUTE_PATHS.HOME },
-  { label: "Professionals", id: "item-nav", propName: ROUTE_PATHS.PROFESIONALES }
+  // { label: "Professionals", id: "item-nav", propName: ROUTE_PATHS.PROFESIONALES }
 
 ]
